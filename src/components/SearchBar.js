e2c53f45cb7f777e16b640bf8837f101ad9a1517
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-16 13:42:17 +0530

    Alternate syntax of event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
-
   render(){
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input type="text" onChange={(e) => console.log(e.target.value)}/>
             </div>
         </form>
       </div>
