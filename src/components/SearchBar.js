cd83909211f906127bb98ad4a21424fabcbec37c
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-26 18:47:50 +0530

    handling context issue - 2(arrow-function)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,14 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = {term:''};
 
-  onFormSubmit(e){
+  onFormSubmit = (e) => {
     e.preventDefault();
     console.log(this.state.term);
   }
   render(){
     return (
       <div className="ui segment">
-        <form onSubmit={e => this.onFormSubmit(e)} className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
