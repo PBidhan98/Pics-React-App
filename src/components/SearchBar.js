95cc5287403ffba0d8e1c8bdd75057711bc88e7e
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-26 18:45:13 +0530

    handling context issue - 1

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,12 @@ class SearchBar extends React.Component {
 
   onFormSubmit(e){
     e.preventDefault();
+    console.log(this.state.term);
   }
-  
   render(){
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={e => this.onFormSubmit(e)} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
