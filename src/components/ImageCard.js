788db54b6599f696c4c029b091ea55772da2ce3a
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-27 18:04:53 +0530

    Dynamic spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,8 @@ import React from 'react';
 class ImageCard extends React.Component{
   constructor(props){
     super(props);
+//initialising state
+    this.state = {spans: 0};
 
     this.imageRef = React.createRef();
   }
@@ -12,13 +14,15 @@ class ImageCard extends React.Component{
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
-  }
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height /10);
+    this.setState({spans});
+  };
 
   render() {
     const {description, urls} = this.props.image;
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}` }}>
         <img
           ref={this.imageRef}
           alt={description}
