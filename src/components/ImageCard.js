696e6a92f40410a3ded26b50ed21908da599faf0
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-27 14:03:16 +0530

    Access DOM with Refs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+
   render() {
+    const {description, urls} = this.props.image;
     return (
       <div>
         <img
-          alt={this.props.image.description}
-          src={this.props.image.urls.regular}
+          ref={this.imageRef}
+          alt={description}
+          src={urls.regular}
         />
       </div>
     );
